b08d1b2ab1957367ff1175c53e25a484727c9db4
Akash Patel <aksp1790@gmail.com>
2024-05-07 19:57:54 +0530

    added few more endpoints

--- src/main/java/org/example/ecommserviceinventory/inventoryservice/controller/InventoryController.java
+++ src/main/java/org/example/ecommserviceinventory/inventoryservice/controller/InventoryController.java
@@ -1,9 +1,15 @@
 package org.example.ecommserviceinventory.inventoryservice.controller;
 
+import org.example.ecommserviceinventory.WebClientConfig;
 import org.example.ecommserviceinventory.inventoryservice.models.InventoryItem;
+import org.example.ecommserviceinventory.inventoryservice.models.Product;
 import org.example.ecommserviceinventory.inventoryservice.repositories.IInventoryRepository;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,10 +19,13 @@ import java.util.UUID;
 @RequestMapping("api/v1/inventory-item")
 public class InventoryController {
     private IInventoryRepository inventoryRepository;
+    private WebClient webClient;
 
-    public InventoryController(IInventoryRepository inventoryRepository)
+    public InventoryController(IInventoryRepository inventoryRepository, WebClient webClient)
     {
         this.inventoryRepository = inventoryRepository;
+        this.webClient = webClient;
+
     }
 
     @GetMapping
@@ -33,9 +42,26 @@ public class InventoryController {
         return ResponseEntity.ok().body(inventoryItem);
     }
 
+    @GetMapping("product/{productId}")
+    public ResponseEntity<Optional<InventoryItem>> getInventoryItemByProductId(@PathVariable String productId)
+    {
+        Optional<InventoryItem> inventoryItem = inventoryRepository.findByProductId(productId).stream().findFirst();
+        return ResponseEntity.ok().body(inventoryItem);
+    }
+
     @PostMapping
     public ResponseEntity<String> createInventoryItem(@RequestBody InventoryItem inventoryItem)
     {
+        if(inventoryItem == null)
+        {
+            return ResponseEntity.badRequest().body("Inventory item cannot be null");
+        }
+
+        if(!isProductExist(inventoryItem.getProductId()))
+        {
+            return ResponseEntity.badRequest().body("Product does not exist");
+        }
+        // Check for the product existance;
         inventoryItem.setId(UUID.randomUUID().toString());
         inventoryRepository.save(inventoryItem);
         return ResponseEntity.ok().body("Inventory item created with id " + inventoryItem.getId());
@@ -82,5 +108,27 @@ public class InventoryController {
         item.get().setQuantity(finalQuantity);
         inventoryRepository.save(item.get());
         return ResponseEntity.ok().body("Inventory item quantity removed with id " + inventoryitemId);
+    }
+
+    private boolean isProductExist(String productId)
+    {
+
+        Product product = webClient.get()
+                .uri("ecomm-service-product/api/v1/products/"+productId)
+                .retrieve().bodyToMono(Product.class)
+                .onErrorResume(WebClientResponseException.class, ex -> {
+                    if (ex.getStatusCode() == HttpStatusCode.valueOf(404)) {
+                        // Handle the "not found" case here
+                        // For example, return a default MyModel or throw an exception
+                        return Mono.empty(); // Return an empty Mono
+                    } else {
+                        // For other errors, rethrow the exception
+                        return Mono.error(ex);
+                    }
+                })
+                .block();
+
+
+        return  product != null;
     }
 }
--- src/main/java/org/example/ecommserviceinventory/inventoryservice/repositories/IInventoryRepository.java
+++ src/main/java/org/example/ecommserviceinventory/inventoryservice/repositories/IInventoryRepository.java
@@ -3,5 +3,9 @@ package org.example.ecommserviceinventory.inventoryservice.repositories;
 import org.example.ecommserviceinventory.inventoryservice.models.InventoryItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface IInventoryRepository extends JpaRepository<InventoryItem,String> {
+    List<InventoryItem> findByProductId(String productId);
 }
